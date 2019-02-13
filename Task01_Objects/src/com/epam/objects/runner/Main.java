702d221680e5373a529d28d783fbb37be956279f
Uladzislaw <vladislav.sergienya@gmail.com>
2019-02-13 11:55:14 +0300

    Test commit

---
+++ Task01_Objects/src/com/epam/objects/runner/Main.java
@@ -0,0 +1,12 @@
+package com.epam.objects.runner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+final class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+}
