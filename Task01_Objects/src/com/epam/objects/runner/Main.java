96da8f3064e282be4594f273b122d4036b2e49b0
Uladzislaw <vladislav.sergienya@gmail.com>
2019-02-13 13:17:02 +0300

    Add java doc for Main

--- Task01_Objects/src/com/epam/objects/runner/Main.java
+++ Task01_Objects/src/com/epam/objects/runner/Main.java
@@ -1,12 +1,55 @@
+/**
+ * Package has one runner class.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
 package com.epam.objects.runner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Class than starts the program.
+ *
+ * @author Vladislav Sergienya
+ * @version 1.0
+ */
+
 final class Main {
+    /**
+     * Logger for recording a program state.
+     */
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    /**
+     * Private constructor without param.
+     */
+    private Main() {
+    }
 
+    /**
+     * Program initial point.
+     * @param args program arguments.
+     */
+    public static void main(final String[] args) {
     }
 }
