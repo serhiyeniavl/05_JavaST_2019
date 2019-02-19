23e74e34761e528c56224de6850f558a003df26d
Uladzislaw <vladislav.sergienya@gmail.com>
2019-02-19 17:25:07 +0300

    Add situation in object test when points are dot

--- Task01_Objects/src/test/com/epam/objects/validator/ObjectValidatorTest.java
+++ Task01_Objects/src/test/com/epam/objects/validator/ObjectValidatorTest.java
@@ -18,31 +18,45 @@ public class ObjectValidatorTest {
         return
                 new Object[][] {
                         {new ArrayList<Double>(Arrays.asList(3.0, 1.0, 0.0,
-                                3.0, 25.0, 0.0, 10.0, 4.0, 4.0))}
+                                3.0, 25.0, 0.0, 10.0, 4.0, 4.0)), true}
                 };
     }
 
     @DataProvider(name = "input_incorrect_data")
-    public Object[][] createIncorrectData() {
+    public Object[][] createIncorrectData1() {
         return
                 new Object[][] {
                         {new ArrayList<Double>(Arrays.asList(3.0, 1.0, 0.0,
-                                3.0, 25.0, 3.0, 10.0, 4.0, 4.0))}
+                                3.0, 25.0, 3.0, 10.0, 4.0, 4.0)), false}
+                };
+    }
+
+    @DataProvider(name = "input_incorrect_data_dot")
+    public Object[][] createIncorrectData2() {
+        return
+                new Object[][] {
+                        {new ArrayList<Double>(Arrays.asList(3.0, 1.0, 0.0,
+                                3.0, 1.0, 0.0, 10.0, 4.0, 4.0)), false}
                 };
     }
 
     @Test(description = "Positive script when input data is correct",
             dataProvider = "input_correct_data")
-    public void testValidate1(List<Double> data) {
-        boolean expected = true;
+    public void testValidate1(List<Double> data, boolean expected) {
         boolean actual = new ObjectValidator(data).validate();
         Assert.assertEquals(actual, expected);
     }
 
     @Test(description = "Negative script when input data is not correct",
             dataProvider = "input_incorrect_data")
-    public void testValidate2(List<Double> data) {
-        boolean expected = false;
+    public void testValidate2(List<Double> data, boolean expected) {
+        boolean actual = new ObjectValidator(data).validate();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Negative script when input data is not correct",
+            dataProvider = "input_incorrect_data_dot")
+    public void testValidate3(List<Double> data, boolean expected) {
         boolean actual = new ObjectValidator(data).validate();
         Assert.assertEquals(actual, expected);
     }
