0e567d7651280a0f9e4c98ffeb8792321eb7d123
Uladzislaw <vladislav.sergienya@gmail.com>
2019-02-22 20:21:40 +0300

    Fix bug when index less than zero

--- Task01_Objects/src/com/epam/objects/entity/Pyramid.java
+++ Task01_Objects/src/com/epam/objects/entity/Pyramid.java
@@ -70,7 +70,7 @@ public class Pyramid implements Geometry {
     public void setPoint(final double coordinateX, final double coordinateY,
                          final double coordinateZ, final int index)
             throws InvalidDataAmountException {
-        if (index > 1) {
+        if (index > 1 || index < 0) {
             LOGGER.error("Invalid data when try to set a point.");
             throw new InvalidDataAmountException("Invalid data.");
         }
