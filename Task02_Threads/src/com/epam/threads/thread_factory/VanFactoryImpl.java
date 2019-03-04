d2e068c6943a209a778dbd27ad121866a27f0a77
Uladzislaw <vladislav.sergienya@gmail.com>
2019-03-04 11:57:08 +0300

    Add factory for creating vans objects from file

---
+++ Task02_Threads/src/com/epam/threads/thread_factory/VanFactoryImpl.java
@@ -0,0 +1,47 @@
+package com.epam.threads.thread_factory;
+
+import com.epam.threads.entity.Van;
+import com.epam.threads.exception.InvalidArgumentException;
+import com.epam.threads.exception.NullArgumentException;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * Class inherited from {@link VanFactory}. Creates map of vans queue.
+ */
+public class VanFactoryImpl implements VanFactory<Van> {
+    /**
+     * Creates {@link Map} that contains priority and van target.
+     * @param data input data.
+     * @return queue of vans {@link Map}.
+     * @throws InvalidArgumentException when van target is invalid.
+     * @throws NullArgumentException when van status is invalid.
+     */
+    @Override
+    public Map<Integer, Queue<Van>> createVanQueueMap(final List<Integer> data)
+            throws InvalidArgumentException, NullArgumentException {
+        Map<Integer, Queue<Van>> vanCallableMapQueue = new HashMap<>();
+        ArrayDeque<Van> commonQueue = new ArrayDeque<>();
+        ArrayDeque<Van> fastQueue = new ArrayDeque<>();
+        final int inputDataAmount = 3;
+        for (int i = 0; i < inputDataAmount - 1; i++) {
+            for (int j = 0; j < data.get(i); j++) {
+                if (j == 0) {
+                    commonQueue.add(new Van("load", "common"));
+                } else {
+                    commonQueue.add(new Van("unload", "common"));
+                }
+            }
+        }
+        for (int i = 0; i < data.get(2); i++) {
+            fastQueue.add(new Van("unload", "express"));
+        }
+        vanCallableMapQueue.put(0, commonQueue);
+        vanCallableMapQueue.put(1, fastQueue);
+        return vanCallableMapQueue;
+    }
+}
