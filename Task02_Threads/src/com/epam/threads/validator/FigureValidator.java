cd5cc1d2da01dc9548d7a8ab27feeeca6100ffdb
Uladzislaw <vladislav.sergienya@gmail.com>
2019-03-04 12:11:00 +0300

    Do little fix and add java doc

--- Task02_Threads/src/com/epam/threads/validator/FigureValidator.java
+++ Task02_Threads/src/com/epam/threads/validator/FigureValidator.java
@@ -2,6 +2,9 @@ package com.epam.threads.validator;
 
 import java.util.List;
 
+/**
+ * Class validating data form input file.
+ */
 public class FigureValidator {
     /**
      * Pattern for check figure.
@@ -13,23 +16,32 @@ public class FigureValidator {
      */
     private static final String STRING_SPLIT_PATTERN = "\\s+";
 
+    /**
+     * Private constructor, to not allow to create object of this class.
+     */
     private FigureValidator() {
-
     }
 
+    /**
+     * Method validating file data. Data is not correct if size not suit or
+     * symbol is not figure.
+     * @param fileData data to check valid.
+     * @return {@code true} if data is valid.
+     */
     public static boolean validate(final List<String> fileData) {
+        final int inputDataLen = 3;
         if (fileData.size() > 1) {
             return false;
         }
         for (String string : fileData) {
-            if (string.split(STRING_SPLIT_PATTERN).length == 3) {
+            if (string.split(STRING_SPLIT_PATTERN).length == inputDataLen) {
                 for (String substring : string.split(STRING_SPLIT_PATTERN)) {
                     if (!substring.matches(FIGURE_PATTERN)) {
                         return false;
                     }
                 }
             }
-            if (string.split(STRING_SPLIT_PATTERN).length != 3) {
+            if (string.split(STRING_SPLIT_PATTERN).length != inputDataLen) {
                 return false;
             }
         }
---
+++ Task02_Threads/src/com/epam/threads/validator/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package contains of validator class.
+ */
+package com.epam.threads.validator;
