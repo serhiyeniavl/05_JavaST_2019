13ef3dd066fabc7b18793d0ef47ba4b72fac91ba
Uladzislaw <vladislav.sergienya@gmail.com>
2019-03-05 16:28:28 +0300

    Add exception for fatal error.

---
+++ Task02_Threads/src/com/epam/threads/exception/FatalErrorRuntimeException.java
@@ -0,0 +1,27 @@
+package com.epam.threads.exception;
+
+/**
+ * Extended class to show a fatal error in program and stops this one.
+ */
+public class FatalErrorRuntimeException extends RuntimeException {
+    /**
+     * Constructor - creates exception with error message.
+     *
+     * @param message exception message.
+     */
+    public FatalErrorRuntimeException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor - creates exception with error message and cause of that
+     * exception.
+     *
+     * @param message exception message.
+     * @param cause   cause of exception.
+     */
+    public FatalErrorRuntimeException(final String message,
+                                      final Throwable cause) {
+        super(message, cause);
+    }
+}
