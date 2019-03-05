fb80741787a75bdc30fee23f74fe12d5a95e1fd9
Uladzislaw <vladislav.sergienya@gmail.com>
2019-03-05 16:27:59 +0300

    Delete useless lock from method.

--- Task02_Threads/src/com/epam/threads/common_resourse/Terminal.java
+++ Task02_Threads/src/com/epam/threads/common_resourse/Terminal.java
@@ -1,30 +1,15 @@
 package com.epam.threads.common_resourse;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Class helper to create terminal in {@link LogisticBaseSingleton}.
- *
- * @see Lock
  */
 class Terminal {
-    /**
-     * Locker.
-     */
-    private Lock locker;
 
     /**
      * Terminal status.
      */
     private boolean isFree = true;
-
-    /**
-     * Constructor - initialize locker.
-     */
-    Terminal() {
-        locker = new ReentrantLock();
-    }
 
     /**
      * Checks terminal status.
@@ -41,8 +26,6 @@ class Terminal {
      * @param free status to set.
      */
     void setFree(final boolean free) {
-        locker.lock();
         isFree = free;
-        locker.unlock();
     }
 }
