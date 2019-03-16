ef8a1326b49fe7206325bacbb71a7022855a4269
Uladzislaw <vladislav.sergienya@gmail.com>
2019-03-16 23:47:49 +0300

    Do little fixes

--- Task03_Information_handling/src/com/epam/info_handling/entity/TextComponentImpl.java
+++ Task03_Information_handling/src/com/epam/info_handling/entity/TextComponentImpl.java
@@ -1,12 +1,19 @@
 package com.epam.info_handling.entity;
 
+import com.epam.info_handling.constant.TextLevel;
 import com.epam.info_handling.exception.InvalidIndexException;
+import com.epam.info_handling.exception.UnsupportedMethodException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class TextComponentImpl implements TextComponent {
+    private static final Logger LOGGER
+            = LogManager.getLogger(TextComponentImpl.class);
+
     private String name;
     private String data;
     private List<TextComponent> components = new ArrayList<>();
@@ -15,59 +22,72 @@ public class TextComponentImpl implements TextComponent {
 
     }
 
-    public TextComponentImpl(String name, String data) {
-        this.name = name;
-        this.data = data;
+    public TextComponentImpl(final String componentName,
+                             final String componentData) {
+        this.name = componentName;
+        this.data = componentData;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getData() {
         return data;
     }
 
-    public void setData(String data) {
-        this.data = data;
+
+    public void add(final TextComponent... textComponents) {
+        this.components.addAll(Arrays.asList(textComponents));
     }
 
-    public void add(TextComponent... components) {
-        this.components.addAll(Arrays.asList(components));
-    }
-
-    public void add(TextComponent component) {
+    public void add(final TextComponent component) {
         components.add(component);
     }
 
-    public void remove(TextComponent component) {
+    public void remove(final TextComponent component) {
         components.remove(component);
     }
 
-    public void remove(TextComponent... components) {
-        this.components.removeAll(Arrays.asList(components));
+    public void remove(final TextComponent... textComponents) {
+        this.components.removeAll(Arrays.asList(textComponents));
     }
 
-    public void acquireComponent(final int level) {
+    public List<String> acquireComponent(final TextLevel level) {
+        List<String> requiredComponents = new ArrayList<>();
+//        TextComponentImpl prevComponent = this;
+//        for (int i = 1; i <= level.getValue(); i++) {
+//            try {
+//                TextComponentImpl currentComponent = prevComponent.getChild(0);
+//                prevComponent = currentComponent;
+//            } catch (InvalidIndexException e) {
+//                LOGGER.error("Invalid index. Out of bound.", e);
+////            } catch (UnsupportedMethodException e) {
+////                LOGGER.error("Invalid method was called.", e);
+////            }
+//            }
+//        }
+//
+//        for (TextComponent component : prevComponent.components) {
+//            requiredComponents.add(component.getData());
+//        }
         try {
-            for (TextComponent component : this.getChild(0).getChild(0).getChild(1).getChild(1).components) {
-                System.out.println(component.getData());
-            }
+            System.out.println(this.getChild(0).getChild(0).getChild(0).getChild(0).getChild(0).getChild(0).getName());
         } catch (InvalidIndexException e) {
             e.printStackTrace();
+        } catch (UnsupportedMethodException e) {
+            e.printStackTrace();
         }
+
+        return requiredComponents;
     }
 
-    public TextComponentImpl getChild(final int index)
+    public TextComponent getChild(final int index)
             throws InvalidIndexException {
         if (index < 0 || index >= components.size()) {
             throw new InvalidIndexException("Index out of bound.");
         }
-        return (TextComponentImpl) components.get(index);
+        return components.get(index);
     }
 
 }
--- Task03_Information_handling/src/com/epam/info_handling/parser/TextParser.java
+++ Task03_Information_handling/src/com/epam/info_handling/parser/TextParser.java
@@ -6,5 +6,7 @@ public class TextParser extends AbstractTextParser {
 
     @Override
     public void parseComponent(final TextComponent textComponent) {
+        //Method has an empty body because of class was created only for start
+        //text parse chain.
     }
 }
