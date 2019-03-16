7950bba2be03a134fb46064b7eace675b73a7054
Uladzislaw <vladislav.sergienya@gmail.com>
2019-03-16 23:42:59 +0300

    Add exception for leaf

---
+++ Task03_Information_handling/src/com/epam/info_handling/exception/UnsupportedMethodException.java
@@ -0,0 +1,7 @@
+package com.epam.info_handling.exception;
+
+public class UnsupportedMethodException extends Exception {
+    public UnsupportedMethodException(String message) {
+        super(message);
+    }
+}
