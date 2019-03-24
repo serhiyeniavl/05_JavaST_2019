148d189ac543538f8df858ac836117b668b4f812
Uladzislaw <vladislav.sergienya@gmail.com>
2019-03-24 21:06:26 +0300

    Add test file for reader. Add java doc in reader test

--- Task03_Information_handling/src/test/com/epam/info_handling/reader/ReaderTest.java
+++ Task03_Information_handling/src/test/com/epam/info_handling/reader/ReaderTest.java
@@ -7,23 +7,39 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 
+/**
+ * Class for test reading from file.
+ */
 public class ReaderTest {
+    /**
+     * Path to correct file.
+     */
     private static final String TEST_FILE_PATH = "data" + File.separator
             + "test_file.txt";
+    /**
+     * Path to incorrect file.
+     */
     private static final String INCORRECT_FILE_PATH = "data" + File.separator
             + "empty_file.txt";
 
+    /**
+     * Test when file path is correct.
+     */
     @Test(description = "Testing data reading.")
     public void readDataTest() {
         Reader reader = new Reader();
-        String expected = "    It is a (111^5|1&2<<(2|5>>2&71))|1200 "
-                + "established fact that a reader will be of a\n"
-                + "page when looking at its layout.\n"
-                + "\tBye.";
+        String expected =
+                "    It is a (111^5|1&2<<(2|5>>2&71))|1200 established fact "
+                        + "that a reader will be of a page when looking at its "
+                        + "layout.\n"
+                        + "    Bye.";
         String actual = reader.readData(TEST_FILE_PATH);
         Assert.assertEquals(actual, expected);
     }
 
+    /**
+     * Test when file path is invalid and program throws RuntimeException.
+     */
     @Test(description = "Testing of invalid file.",
             expectedExceptions = FatalRuntimeException.class)
     public void readInvalidDataTest() {
