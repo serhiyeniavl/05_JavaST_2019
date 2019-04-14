322b41cf04afe7c46c9bb0b18c151fca24b9a71b
Uladzislaw <vladislav.sergienya@gmail.com>
2019-04-14 17:50:40 +0300

    Add user dao interface to extend basic interface and add new method

---
+++ Task05_inform_system/src/main/java/by/training/info_system/dao/UserDao.java
@@ -0,0 +1,9 @@
+package by.training.info_system.dao;
+
+import by.training.info_system.entity.User;
+
+import java.util.Optional;
+
+public interface UserDao extends Dao<User> {
+    Optional<User> findUserToSignIn(final String email, final String pass);
+}
