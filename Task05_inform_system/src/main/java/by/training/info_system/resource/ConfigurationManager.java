ad3b08cde4f7465d2cbb031fd259ddd1a67b8437
Uladzislaw <vladislav.sergienya@gmail.com>
2019-04-14 17:52:17 +0300

    Add conf manager to provide info for jsp file paths

---
+++ Task05_inform_system/src/main/java/by/training/info_system/resource/ConfigurationManager.java
@@ -0,0 +1,18 @@
+package by.training.info_system.resource;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ConfigurationManager {
+//    private static final ResourceBundle resource = ResourceBundle.getBundle("WEB-INF/classes/app_config");
+//
+//    private ConfigurationManager() {
+//
+//    }
+//
+//    public static String getProperty(final String key) {
+//        return resource.getString(key);
+//    }
+    public static final String indexPage = "/jsp/index.jsp";
+    public static final String signinPage = "/jsp/signin.jsp";
+}
