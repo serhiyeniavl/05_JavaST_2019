aeecbe3db5b835f89c99fc32ab12db418e47e535
Uladzislaw <vladislav.sergienya@gmail.com>
2019-04-24 19:19:57 +0300

    Add db config connection provider

--- Task05_inform_system/src/main/java/by/training/info_system/resource/DBConfigurationProvider.java
+++ Task05_inform_system/src/main/java/by/training/info_system/resource/DBConfigurationProvider.java
@@ -1,19 +1,36 @@
 package by.training.info_system.resource;
 
-import java.util.ResourceBundle;
+import lombok.extern.log4j.Log4j2;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Log4j2
 public class DBConfigurationProvider {
-//    private static final ResourceBundle resource = ResourceBundle.getBundle("resources/db_connection");
-//
-//    private DBConfigurationProvider() {
-//    }
-//
-//    public static String getProperty(final String key) {
-//        return resource.getString(key);
-//    }
+    private static Properties dbProp;
+    private static DBConfigurationProvider instance;
 
-    public static final String dbDriver = "com.mysql.cj.jdbc.Driver";
-    public static final String dbUser = "manual_user";
-    public static final String dbPassword = "password";
-    public static final String dbUrl = "jdbc:mysql://localhost:3306/car_rent_system_db?useUnicode=true&characterEncoding=UTF8&useSSL=false";
+
+    private DBConfigurationProvider() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("db_connection.properties");
+        try {
+            dbProp.load(inputStream);
+        } catch (IOException e) {
+            log.error("Error loading property file.", e);
+        }
+    }
+
+    public static DBConfigurationProvider getInstance() {
+        if (instance == null) {
+            dbProp = new Properties();
+            instance = new DBConfigurationProvider();
+        }
+        return instance;
+    }
+
+    public String getProperty(final String key) {
+        return dbProp.getProperty(key);
+    }
 }
