6b1ffb7a62b14465a25a14d8a48d7f5757520507
Uladzislaw <vladislav.sergienya@gmail.com>
2019-04-29 01:03:50 +0300

    Add abstract dao with common methods

---
+++ Task05_inform_system/src/main/java/by/training/info_system/dao/AbstractDao.java
@@ -0,0 +1,81 @@
+package by.training.info_system.dao;
+
+import by.training.info_system.dao.connection_pool.ConnectionPool;
+import lombok.extern.log4j.Log4j2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Log4j2
+public abstract class AbstractDao {
+    protected static final String RESULT_SET_ERROR = "Result set error: ";
+
+
+    private Connection connection;
+
+    protected AbstractDao() {
+        this.connection = ConnectionPool.getInstance().getConnection();
+    }
+
+    protected void returnConnectionInPool() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            log.error("Cannot return connection in pool", e);
+        }
+    }
+
+    protected Statement createStatement() {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            log.error("Error when try to create statement", e);
+        }
+        return statement;
+    }
+
+    public PreparedStatement createPreparedStatement(final String sql) {
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            log.error("Error when try to create prepare statement", e);
+        }
+        return statement;
+    }
+
+    protected void closeStatement(final Statement statement) {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (SQLException e) {
+            log.error("Cannot close statement", e);
+        }
+    }
+
+    public void closePreparedStatement(final PreparedStatement statement) {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (SQLException e) {
+            log.error("Cannot close prepared statement", e);
+        }
+    }
+
+//    public void closeResultSet(final ResultSet resultSet) {
+//        try {
+//            if (resultSet != null) {
+//                resultSet.close();
+//            }
+//        } catch (SQLException e) {
+//            log.error("Cannot close result set", e);
+//        }
+//    }
+}
+
