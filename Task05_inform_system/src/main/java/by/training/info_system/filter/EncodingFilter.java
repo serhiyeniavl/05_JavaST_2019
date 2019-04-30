07b9bdde057b929a7a55b1dcc5a8c396ec609efd
Uladzislaw <vladislav.sergienya@gmail.com>
2019-04-30 23:57:37 +0300

    Add encoding filter

---
+++ Task05_inform_system/src/main/java/by/training/info_system/filter/EncodingFilter.java
@@ -0,0 +1,31 @@
+package by.training.info_system.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+    private String encoding = "utf-8";
+
+
+    @Override
+    public void init(final FilterConfig filterConfig) {
+        String encodingParam = filterConfig.getInitParameter("encoding");
+        if (encodingParam != null) {
+            encoding = encodingParam;
+        }
+    }
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest,
+                         final ServletResponse servletResponse,
+                         final FilterChain filterChain)
+            throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(encoding);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
