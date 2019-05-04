a90444f9dad94972dc17443897a8cd5ece269c2f
Uladzislaw <vladislav.sergienya@gmail.com>
2019-05-04 23:27:01 +0300

    Now command checks user on authorization

--- Task05_inform_system/src/main/java/by/training/info_system/command/EmptyCommand.java
+++ Task05_inform_system/src/main/java/by/training/info_system/command/EmptyCommand.java
@@ -1,10 +1,29 @@
 package by.training.info_system.command;
 
+import by.training.info_system.resource.page.JspPage;
+import by.training.info_system.resource.page.PageEnum;
+import by.training.info_system.resource.page.PageFactory;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class EmptyCommand extends Command {
     @Override
-    public String execute(HttpServletRequest request) {
-        return null;
+    public JspPage execute(final HttpServletRequest request, final HttpServletResponse response) {
+        String requestedPage = (String) request.getAttribute("requestedPage");
+        JspPage page = PageFactory.defineAndGet(PageEnum.valueOf(requestedPage.toUpperCase()));
+
+        if (isAuthorizedUserTrySignAgain(request, page)) {
+            page = PageFactory.defineAndGet(PageEnum.HOME);
+            page.setRedirect(true);
+        }
+        return page;
+    }
+
+    private boolean isAuthorizedUserTrySignAgain(final HttpServletRequest request, JspPage page) {
+        return (request.getSession(false) != null
+                && request.getSession(false).getAttribute("user") != null
+                && (page.getUri().equals(PageEnum.SIGNIN.getUri())
+                || page.getUri().equals((PageEnum.SIGNUP.getUri()))));
     }
 }
