b784e2afabd8c0c8e7abb850d852975789773d4b
Uladzislaw <vladislav.sergienya@gmail.com>
2019-05-05 21:34:26 +0300

    Add transactions

--- Task05_inform_system/src/main/java/by/training/info_system/dao/DaoCreator.java
+++ Task05_inform_system/src/main/java/by/training/info_system/dao/DaoCreator.java
@@ -5,5 +5,8 @@ import java.util.Optional;
 public interface DaoCreator {
     <T extends Dao<?>> Optional<T> createDao(final Class<T> key);
 
+    void autoCommit(boolean commit);
+    void commit();
+    void rollback();
     void close();
 }
