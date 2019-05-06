2f2a8b5fc9364b773c15b009b9050d1b5a3de7c7
Uladzislaw <vladislav.sergienya@gmail.com>
2019-05-06 23:39:51 +0300

    Add check on incorrect data

--- Task05_inform_system/src/main/java/by/training/info_system/command/SignUpCommand.java
+++ Task05_inform_system/src/main/java/by/training/info_system/command/SignUpCommand.java
@@ -1,5 +1,6 @@
 package by.training.info_system.command;
 
+import by.training.info_system.command.client.RequestAttribute;
 import by.training.info_system.entity.Passport;
 import by.training.info_system.entity.User;
 import by.training.info_system.entity.data.UserData;
@@ -9,11 +10,11 @@ import by.training.info_system.resource.page.PageEnum;
 import by.training.info_system.resource.page.PageFactory;
 import by.training.info_system.service.UserService;
 import by.training.info_system.util.PasswordHasher;
+import by.training.info_system.validator.UserValidator;
 import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -36,36 +37,64 @@ public class SignUpCommand extends Command {
 
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-        Passport userPassport = Passport.builder()
-                .serie(passportNumber.substring(0, 2))
-                .number(Integer.valueOf(passportNumber.substring(2)))
-                .idNumber(passportIdNumber)
-                .issueDate(LocalDate.parse(passportIssueDate, timeFormatter))
-                .endDate(LocalDate.parse(passportEndDate, timeFormatter))
-                .build();
+        User user;
+        Passport userPassport;
+        UserData userData;
+        try {
+            userPassport = Passport.builder()
+                    .serie(passportNumber.substring(0, 2))
+                    .number(Integer.valueOf(passportNumber.substring(2)))
+                    .idNumber(passportIdNumber)
+                    .issueDate(LocalDate.parse(passportIssueDate, timeFormatter))
+                    .endDate(LocalDate.parse(passportEndDate, timeFormatter))
+                    .build();
 
-        UserData userData = UserData.builder()
-                .fName(fName)
-                .lName(lName)
-                .passport(userPassport)
-                .address(address)
-                .build();
+            userData = UserData.builder()
+                    .fName(fName)
+                    .lName(lName)
+                    .passport(userPassport)
+                    .address(address)
+                    .build();
 
-        User user = User.builder()
-                .login(email)
-                .password(PasswordHasher.hashPass(password))
-                .role(Role.USER)
-                .userData(userData)
-                .build();
+            user = User.builder()
+                    .login(email)
+                    .password(PasswordHasher.hashPass(password))
+                    .role(Role.USER)
+                    .userData(userData)
+                    .build();
+        } catch (Exception e) {
+            putAttrInRequest(request, RequestAttribute.INCORRECT_DATA,
+                    "You've incorrectly filled the form. Check the tips inside" +
+                            " fields.");
+            return page;
+        }
 
+        if (!validate(new UserValidator(), user)) {
+            putAttrInRequest(request, RequestAttribute.INCORRECT_DATA,
+                    "You've incorrectly filled the form. Check the tips inside" +
+                            " fields.");
+            return page;
+        }
 
         UserService service = factory.getService(UserService.class).orElseThrow();
+        if (service.isInBlackList(user)) {
+            page = PageFactory.defineAndGet(PageEnum.SIGNIN);
+            putAttrInRequest(request, RequestAttribute.BLACK_LIST,
+                    "This account already exists and have been banned.");
+            return page;
+        }
+        if (service.isExist(userPassport.getNumber(), userPassport.getIdNumber())) {
+            page = PageFactory.defineAndGet(PageEnum.SIGNIN);
+            putAttrInRequest(request, RequestAttribute.INCORRECT_DATA,
+                    "User with that passport data already exists. Try to sign in.");
+            return page;
+        }
         boolean isCreated = service.registerNewUser(user);
         if (isCreated) {
             page = PageFactory.defineAndGet(PageEnum.SIGNIN);
-            putAttrInRequest(request,"signedUp",
+            putAttrInRequest(request, RequestAttribute.SIGNED_UP,
                     "You've successfully signed up!\nPlease, sign in" +
-                    " into your account.");
+                            " into your account.");
         }
         return page;
     }
