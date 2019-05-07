35618aee84bebae345557b1089bf6c03d2001ad6
Uladzislaw <vladislav.sergienya@gmail.com>
2019-05-07 23:03:52 +0300

    Add loading cars from db

--- Task05_inform_system/src/main/java/by/training/info_system/command/EmptyCommand.java
+++ Task05_inform_system/src/main/java/by/training/info_system/command/EmptyCommand.java
@@ -1,11 +1,16 @@
 package by.training.info_system.command;
 
+import by.training.info_system.command.client.RequestAttribute;
+import by.training.info_system.entity.Car;
 import by.training.info_system.resource.page.JspPage;
 import by.training.info_system.resource.page.PageEnum;
 import by.training.info_system.resource.page.PageFactory;
+import by.training.info_system.service.CarService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmptyCommand extends Command {
     @Override
@@ -16,6 +21,12 @@ public class EmptyCommand extends Command {
         if (isAuthorizedUserTrySignAgain(request, page)) {
             page = PageFactory.defineAndGet(PageEnum.HOME);
             page.setRedirect(true);
+        }
+
+        if (page.getUri().equals(PageEnum.CARS.getUri())) {
+            CarService service = factory.getService(CarService.class).orElseThrow();
+            List<Car> cars = service.loadCars().orElseGet(ArrayList::new);
+            putAttrInRequest(request, RequestAttribute.CARS, cars);
         }
         return page;
     }
