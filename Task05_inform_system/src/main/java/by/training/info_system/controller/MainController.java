ff823d38109d37c2c94737a1b976d47751a8febf
Uladzislaw <vladislav.sergienya@gmail.com>
2019-05-11 20:52:18 +0300

    Divide get and post queries

--- Task05_inform_system/src/main/java/by/training/info_system/controller/MainController.java
+++ Task05_inform_system/src/main/java/by/training/info_system/controller/MainController.java
@@ -1,9 +1,6 @@
 package by.training.info_system.controller;
 
-import by.training.info_system.command.ActionCommandFactory;
-import by.training.info_system.command.Command;
-import by.training.info_system.command.CommandManager;
-import by.training.info_system.command.CommandManagerFactory;
+import by.training.info_system.command.*;
 import by.training.info_system.dao.connection_pool.ConnectionPool;
 import by.training.info_system.dao.impl.DaoManagerImpl;
 import by.training.info_system.resource.page.JspPage;
@@ -24,34 +21,38 @@ public class MainController extends HttpServlet {
         ConnectionPool.getInstance();
     }
 
-    @Override
-    protected void doPost(final HttpServletRequest request,
-                          final HttpServletResponse response) {
-        processRequest(request, response);
-    }
 
     @Override
-    protected void doGet(final HttpServletRequest request,
-                         final HttpServletResponse response) {
-        processRequest(request, response);
-    }
-
-    private void processRequest(final HttpServletRequest req,
-                                final HttpServletResponse resp) {
-        Command command = ActionCommandFactory.defineCommand(req);
+    protected void doGet(final HttpServletRequest req,
+                         final HttpServletResponse resp) throws IOException {
         CommandManager commandManager = CommandManagerFactory.getManager(new ServiceFactoryImpl(new DaoManagerImpl()));
-
-        JspPage redirectPage = commandManager.execute(command, req, resp);
+        JspPage redirectPage = commandManager.execute(new EmptyCommand(), req, resp);
         commandManager.close();
 
         try {
             if (redirectPage.isRedirect()) {
-                resp.sendRedirect(resp.encodeRedirectURL(req.getContextPath() + "/" + redirectPage.getUri()));
+                redirectPage.setRedirect(false);
+                resp.sendRedirect(resp.encodeRedirectURL(req.getContextPath() + "/" + redirectPage.getUri() + redirectPage.getRequestParameters()));
             } else {
                 getServletContext().getRequestDispatcher(redirectPage.getJspPagePath()).forward(req, resp);
             }
         } catch (ServletException e) {
             log.error("Servlet exception when forward req and resp", e);
+        } catch (IOException e) {
+            log.error("Servlet IO exception", e);
+        }
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest req,
+                          final HttpServletResponse resp) {
+        Command command = ActionCommandFactory.defineCommand(req);
+        CommandManager commandManager = CommandManagerFactory.getManager(new ServiceFactoryImpl(new DaoManagerImpl()));
+        JspPage redirectPage = commandManager.execute(command, req, resp);
+        commandManager.close();
+
+        try {
+            resp.sendRedirect(resp.encodeRedirectURL(req.getContextPath() + "/" + redirectPage.getUri() + redirectPage.getRequestParameters()));
         } catch (IOException e) {
             log.error("Servlet IO exception", e);
         }
